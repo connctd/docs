89aa1b7de47fd3be26622fa4ad8d4a1878d5c980
Pandelis Zembashis <info@pandelis.me>
2020-10-06 13:10:56 +0200

    add: apikey generator

--- api/querykey.js
+++ api/querykey.js
@@ -4,6 +4,10 @@ const clientId = process.env.CONNCTD_CLIENT_ID
 const clientSecret = process.env.CONNCTD_CLIENT_SECRET
 
 module.exports.default = (req, res) => {
+    if (req.method === "OPTIONS") {
+        res.status(200).end()
+        return
+    }
     axios.post("https://api.connctd.io/oauth2/token", "grant_type=client_credentials&scopes=connctd.things.read%20connctd.units.read", {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -11,12 +15,24 @@ module.exports.default = (req, res) => {
             Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString("base64")}`,
         },
     }).then((accessRes) => {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.setHeader("Access-Control-Allow-Methods", "GET")
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        )
         res.json({
             status: "ok",
             key: accessRes.data.access_token,
         })
     }).catch((err) => {
         res.status(500)
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.setHeader("Access-Control-Allow-Methods", "GET")
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        )
         console.log({ req, err })
         res.json({
             status: "error",
---
+++ docs/components/KeyUtilities.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useCallback, useEffect } from "react"
+import { Input, SpiNNer } from "@connctd/quartz"
+import { VscRefresh } from "react-icons/vsc"
+import BeatLoader from "react-spinners/BeatLoader"
+
+
+export function KeyUtilities() {
+    const [key, setkey] = useState("")
+    const [pending, setPending] = useState(true)
+
+    const fetchKey = useCallback(() => {
+        setPending(true)
+        setkey("...")
+        fetch("https://docs-pandelis.connctd.vercel.app/api/querykey")
+            .then(response => response.json())
+            .then((json) => {
+                setPending(false)
+                setkey(json.key)
+            })
+            .catch((err) => {
+            //eslint-disable-next-line
+                console.error(err)
+                return err
+            })
+    }, [key, pending])
+
+    useEffect(() => {
+        fetchKey()
+    }, [])
+
+    return (
+        <div>
+            <Input
+                readOnly
+                icon={pending ? <BeatLoader size={5} margin={0} color="white" /> : <VscRefresh onClick={() => { fetchKey() }} color="white" size="1.5em" />}
+                type="text"
+                value={key}
+            />
+
+            <h3>Using the apollo CLI?</h3>
+
+            <code>
+$ apollo schema:download --endpoint=https://api.connctd.io/api/v1/query --header=&quot;Authorization: Bearer
+                {" "}
+                {key}
+                &quot; connctdSchema.json
+            </code>
+
+            <h4>Generate Types</h4>
+            <code>
+$ apollo client:codegen --target=typescript --endpoint=https://api.connctd.io/api/v1/query --header=&quot;Authorization: Bearer
+                {" "}
+                {key}
+                &quot; types
+            </code>
+        </div>
+    )
+}
+
+
+export default KeyUtilities
